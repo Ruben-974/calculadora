45d107a7c620d5c76e453dd4ce5404876ab736f5
Rúben Israel <87450219+Ruben-974@users.noreply.github.com>
2024-01-31 12:16:57 -0300

    layout

---
+++ files/main.py
@@ -0,0 +1,31 @@
+import PySimpleGUI as sg
+
+def create_decorator(value):
+
+    def create_function(funct):
+
+        def inner(txt):
+
+            return funct(txt, value)
+
+        return inner
+    
+    return create_function
+
+@create_decorator(7)
+
+def model_button(txt, num):
+
+    return sg.Button(txt, size=num)
+
+layout = [[sg.Input(size=40, justification='right')],
+          [model_button('7'), model_button('8'), model_button('9'), model_button('+')],
+          [model_button('4'), model_button('5'), model_button('6'), model_button('-')],
+          [model_button('1'), model_button('2'), model_button('3'), model_button('*')],
+          [model_button('0'), model_button('='), model_button(','), model_button('/')]
+          ]
+
+window = sg.Window('Calculator', layout)
+evente, value = window.read()
+
+print(evente, value)
